6eca004cc9d72ad12abdb0f6b88bbf779e0b71cc
Jersonrn <jersonrn@gmail.com>
2024-09-07 20:49:21 -0500

    srcRect and destRect implemented

--- include/Texture.hpp
+++ include/Texture.hpp
@@ -1,12 +1,17 @@
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
+#include <string>
 
 
 class Texture {
     public:
         Texture();
         ~Texture();
-        int load_from_file(SDL_Renderer *renderer, const char *pathFile);
+        int load_from_file(
+                SDL_Renderer *renderer, const std::string &pathFile,
+                int srcRectX = 0, int srcRectY = 0, int srcRectW = -1, int srcRectH = -1,
+                int destRectX = 0, int destRectY = 0, int destRectW = -1, int destRectH = -1
+                );
         void render(SDL_Renderer *renderer);
 
         SDL_Texture *texture;
--- src/Texture.cpp
+++ src/Texture.cpp
@@ -2,23 +2,39 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_render.h>
 #include <Texture.hpp>
-#include <cstddef>
 #include <iostream>
+#include <string>
 
 
-Texture::Texture(){}
+Texture::Texture() : texture(nullptr), srcRect{ 0, 0, 32, 32 }, destRect{ 0, 0, 32, 32 }{}
 Texture::~Texture(){
     SDL_DestroyTexture(texture);
     texture = nullptr;
 }
 
-int Texture::load_from_file(SDL_Renderer *renderer, const char *pathFile){
-    SDL_Surface *tmpSurface = IMG_Load(pathFile);
+int Texture::load_from_file(
+        SDL_Renderer *renderer, const std::string &pathFile,
+        int srcRectX, int srcRectY, int srcRectW, int srcRectH,
+        int destRectX, int destRectY, int destRectW, int destRectH) {
+
+    if (!renderer) {
+        std::cerr << "Renderer is null" << std::endl;
+        return -1;
+    }
+
+    SDL_Surface *tmpSurface = IMG_Load(pathFile.c_str());
     if (tmpSurface){
         texture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
         SDL_FreeSurface(tmpSurface);
 
-        if (!texture) {
+        if (texture) {
+            SDL_QueryTexture(texture, nullptr, nullptr, srcRectW == -1 ? &srcRectW : nullptr, srcRectH == -1 ? &srcRectH : nullptr);
+            SDL_QueryTexture(texture, nullptr, nullptr, destRectW == -1 ? &destRectW : nullptr, destRectH == -1 ? &destRectH : nullptr);
+
+            srcRect = { srcRectX, srcRectY, srcRectW, srcRectH };
+            destRect = { destRectX, destRectY, destRectW, destRectH };
+
+        } else {
             std::cerr << "Failed to create texture. Error: " << SDL_GetError() << std::endl;
             return -1;
         }
@@ -28,9 +44,10 @@ int Texture::load_from_file(SDL_Renderer *renderer, const char *pathFile){
         return -1;
     }
 
+
     return 0;
 }
 
 void Texture::render(SDL_Renderer *renderer){
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderCopy(renderer, texture, &srcRect, &destRect);
 }
