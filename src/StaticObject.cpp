a64ef591d71239bccfb1e18addb09ad9e963ed59
Jersonrn <jersonrn@gmail.com>
2024-09-11 17:08:35 -0500

    WIP: StaticObject class started

---
+++ src/StaticObject.cpp
@@ -0,0 +1,11 @@
+#include "GameObject.hpp"
+#include <StaticObject.hpp>
+
+
+StaticObject::StaticObject(
+        const std::string &pathFile,
+        float xPos, float yPos,
+        float xScale_, float yScale_,
+        int srcRectX, int srcRectY, int srcRectW, int srcRectH
+        ) : GameObject(pathFile, xPos, yPos, xScale_, yScale_, srcRectX, srcRectY, srcRectW, srcRectH){
+}
